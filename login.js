403dc2597e2de2982d6569f3d2a289461f29b1ee
Marcela Paz <marcela.fpaz.s@gmail.com>
2018-07-25 18:09:37 -0400

    estilos

---
+++ login.js
@@ -0,0 +1,17 @@
+(function(){
+  let preload =document.getElementById("preload");
+  let loading = 0;
+  let id = setInterval(frame,64);
+
+  function frame (){
+    if(loading == 100){
+      clearInterval(id);
+      window.open("index.html");
+    }else{
+      loading =loading +1;
+      if(loading == 90){
+        preload.style.animation="fadeout is ease";
+      }
+    }
+  }
+})
